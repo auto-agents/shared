03cab86563c1ff52042b7392cb96ae3d533c4201
franck gaspoz <franck.gaspoz@gmail.com>
2026-03-27 01:41:25 +0100

    imprv sentence splitter

--- src/utils/text.js
+++ src/utils/text.js
@@ -1,7 +1,7 @@
 export const splitSentence = (ctx, text) => {
     const conf = ctx.dialoger.sentenceSpliter
     const sc = conf.splitChars
-    const t = []
+    var t = []
     var x = 0
 
     for (var i = 0; i < conf.replacePatterns.length; i++) {
@@ -37,7 +37,39 @@ export const splitSentence = (ctx, text) => {
     }
     if (x < text.length)
         t.push(text.substring(x).trim())
-    return t.filter(x => x.length > 0)
+    t = t.filter(x => x.length > 0)
+
+    const t2 = []
+    var k = 0
+    while (k < t.length) {
+        const s = t[k]
+        if (s.length == 0) {
+            t2.push(s)
+            k++
+        }
+        else {
+            if (s.length == 1) {
+                if (conf.includeAfterSplit.include(s[0])) {
+                    k++
+                    if (k < t.length) {
+                        t2.push(s[0] + t[k])
+                        k++
+                    }
+                    else
+                        t2.push(s)
+                }
+                else {
+                    t2.push(s)
+                    k++
+                }
+            }
+            else {
+                t2.push(s)
+                k++
+            }
+        }
+    }
+    return t2
 }
 
 export default {
