d977c5117c2f9529c1f15b3b7d4c46971be79494
franck gaspoz <franck.gaspoz@gmail.com>
2026-04-06 01:12:52 +0200

    impl. partial model skill

--- src/data/skill.js
+++ src/data/skill.js
@@ -3,4 +3,17 @@
  */
 export default class Skill {
 
+	name = null
+	description = null
+	location = null
+	metadata = null
+
+	constructor(name, description, location, metadata) {
+		this.name = name
+		this.description = description
+		this.location = location
+		// TODO: from metadata...
+		this.metadata = null
+	}
+
 }
