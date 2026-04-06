d4e1307ef1b8bdbf53fdd36c3a67a4e3b42fd8d3
franck gaspoz <franck.gaspoz@gmail.com>
2026-04-06 18:40:16 +0200

    ouai

--- src/data/dialog-context.js
+++ src/data/dialog-context.js
@@ -3,59 +3,68 @@ import OutputContext from "./output-context"
 
 export default class DialogContext {
 
-    dialoger = null
-    agent = null
-    task = null
-    round = null
+	dialoger = null
+	agent = null
+	task = null
+	round = null
 
-    previousTasks = []
+	userOutputContext = null
+	aiOutputContext = null
 
-    /**
-     * build a new DialogContext necessary to initiale a dialog
-     * @param {OutputContext} outputContext current output context
-     * @param {Dialoger} dialoger dialoger to be used
-     * @param {Object} agent loaded agent specification (ctx.components.agents.agents)
-     * @param {Object} fromAgent dialog initiator if is afent or null if from user, loaded agent specification (ctx.components.agents.agents)
-     * @param {Object} task running task if any
-     * @param {number} round round number
-     */
-    constructor(
-        outputContext,
-        dialoger,
-        agent,
-        fromAgent = null,
-        task = null,
-        round = 1
-    ) {
-        this.outputContext = outputContext
-        this.dialoger = dialoger
-        this.agent = agent
-        this.fromAgent = fromAgent
-        this.task = task
-        this.round = round
-    }
+	previousTasks = []
 
-    clone() {
-        return new DialogContext(
-            this.outputContext,
-            this.dialoger,
-            this.agent,
-            this.fromAgent,
-            this.task,
-            this.round
-        )
-    }
+	/**
+	 * build a new DialogContext necessary to initiale a dialog
+	 * @param {OutputContext} outputContext current output context
+	 * @param {Dialoger} dialoger dialoger to be used
+	 * @param {Object} agent loaded agent specification (ctx.components.agents.agents)
+	 * @param {Object} fromAgent dialog initiator if is afent or null if from user, loaded agent specification (ctx.components.agents.agents)
+	 * @param {Object} task running task if any
+	 * @param {number} round round number
+	 */
+	constructor(
+		outputContext,
+		dialoger,
+		agent,
+		fromAgent = null,
+		task = null,
+		round = 1,
+		userOutputContext = null,
+		aiOutputContext = null
+	) {
+		this.outputContext = outputContext
+		this.dialoger = dialoger
+		this.agent = agent
+		this.fromAgent = fromAgent
+		this.task = task
+		this.round = round
+		this.userOutputContext = userOutputContext
+		this.aiOutputContext = aiOutputContext
+	}
 
-    nextRound() {
-        this.round++
-        return this
-    }
+	clone() {
+		return new DialogContext(
+			this.outputContext,
+			this.dialoger,
+			this.agent,
+			this.fromAgent,
+			this.task,
+			this.round,
+			this.userOutputContext,
+			this.aiOutputContext
+		)
+	}
 
-    setCurrentTask(task) {
-        if (this.task != null)
-            this.previousTasks.push(this.task)
-        this.task = task
-        return this
-    }
+	nextRound() {
+		this.round++
+		return this
+	}
+
+	setCurrentTask(task) {
+		if (this.task != null)
+			this.previousTasks.push(this.task)
+		this.task = task
+		return this
+	}
 
 }
--- src/data/output-context.js
+++ src/data/output-context.js
@@ -1,34 +1,38 @@
 export default class OutputContext {
 
-    y0 = 0
-    y1 = 0
+	y0 = 0
+	y1 = 0
+	x = null
+	y = null
 
-    constructor(ctx, output, margin = 0) {
-        this.ctx = ctx
-        this.output = output
-        this.margin = margin
-        this.marginBase = margin
-        this.y0 = output.getSource().rows.length
-    }
+	constructor(ctx, output, margin = 0, x = null, y = null) {
+		this.ctx = ctx
+		this.output = output
+		this.margin = margin
+		this.marginBase = margin
+		this.y0 = output.getSource().rows.length
+		this.x = x
+		this.y = y
+	}
 
-    clone() {
-        const o = new OutputContext(this.ctx, this.output, this.margin)
-        o.y0 = this.y0
-        o.y1 = this.y1
-        return o
-    }
+	clone() {
+		const o = new OutputContext(this.ctx, this.output, this.margin)
+		o.y0 = this.y0
+		o.y1 = this.y1
+		return o
+	}
 
-    addMargin(margin = 4) {
-        this.margin += margin
-        return this
-    }
+	addMargin(margin = 4) {
+		this.margin += margin
+		return this
+	}
 
-    addMargins(repeat, margin = 4) {
-        this.margin += repeat * margin
-        return this
-    }
+	addMargins(repeat, margin = 4) {
+		this.margin += repeat * margin
+		return this
+	}
 
-    getMargin() {
-        return ' '.repeat(this.margin)
-    }
+	getMargin() {
+		return ' '.repeat(this.margin)
+	}
 }
