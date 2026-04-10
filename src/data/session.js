20ccda3e756b8858e7e66a05e65d6f2f930c65b2
franck gaspoz <franck.gaspoz@gmail.com>
2026-04-10 04:52:11 +0200

    session model

---
+++ src/data/session.js
@@ -0,0 +1,20 @@
+export default class Session {
+
+	// command history
+
+	// loaded plugins / agents
+
+	// agents config ? (prompt, instruct, ...)
+
+	constructor(ctx) {
+		this.ctx = ctx
+	}
+
+	static load(file) {
+
+	}
+
+	save() {
+
+	}
+}
